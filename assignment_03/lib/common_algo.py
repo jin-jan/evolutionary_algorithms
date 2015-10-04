b0089ce21dd734d68bc07dd0d6e44a8d7cd8cea4
jindom <jindrizka.dom@gmail.com>
2015-10-03 23:20:09 -0500

    agregando common_algorithm

---
+++ assignment_03/lib/common_algo.py
@@ -0,0 +1,11 @@
+import math
+
+
+def fitness_func(chromosome):
+    firstSum = 0.0
+    secondSum = 0.0
+    for c in chromosome:
+        firstSum += c**2.0
+        secondSum += math.cos(2.0*math.pi*c)
+    n = float(len(chromosome))
+    return -20.0*math.exp(-0.2*math.sqrt(firstSum/n)) - math.exp(secondSum/n) + 20 + math.e
